5852a0104b3be2a9a7a00be0364776093e48176d
jrh3k5 <jrh3k5@8f5803f9-4034-c7d5-2cc9-b0d18fdb8128>
2011-04-25 03:26:06 +0000

    Added Javadoc, removed unnecessary resources folders

--- trunk/openid-discovery-maven-plugin/src/main/java/com/google/code/openid/mojo/DiscoveredService.java
+++ trunk/openid-discovery-maven-plugin/src/main/java/com/google/code/openid/mojo/DiscoveredService.java
@@ -4,77 +4,153 @@ import java.util.regex.Pattern;
 
 /**
  * Definition of a service that is served as a service in a discovery document.
+ * <p />
+ * The following parameters are <b>required</b> to be set:
+ * <ul>
+ * <li>{@link #setHostRegex(String) hostRegex}</li>
+ * <li>{@link #setUri(String) uri}</li>
+ * <li>{@link #setType(String) type}</li>
+ * </ul>
  * 
  * @author jrh3k5
  * 
  */
 
-// TODO: Javadoc
+// TODO: unit tests
 public class DiscoveredService {
     private Pattern hostRegex;
     private String uri;
     private String type;
     private Integer priority;
     private String localId;
-    
-    public void setHostRegex(String hostRegex) {
-        if(hostRegex == null)
-            throw new IllegalArgumentException("Host regular expression cannot be null.");
-        
-        this.hostRegex = Pattern.compile(hostRegex);
-    }
-    
+
+    /**
+     * Get the regular expression used to match against the request path.
+     * 
+     * @return A {@link Pattern} to be used to match against the request path.
+     * @throws IllegalStateException
+     *             If the regular expression has not yet been {@link #setHostRegex(String)}.
+     */
     public Pattern getHostRegex() {
-        if(hostRegex == null)
+        if (hostRegex == null)
             throw new IllegalStateException("Host regular expression has not yet been set prior to retrieval.");
-        
+
         return hostRegex;
     }
-    
-    public void setUri(String uri) {
-        if(uri == null)
-            throw new IllegalArgumentException("URI cannot be null.");
-        
-        this.uri = uri;
+
+    /**
+     * Get the local ID.
+     * 
+     * @return The local ID. This can be {@code null}.
+     */
+    public String getLocalId() {
+        return localId;
     }
-    
-    public String getUri() {
-        if(uri == null)
-            throw new IllegalStateException("URI has not yet been set prior to retrieval.");
-        
-        return uri;
-    }
-    
-    public void setType(String type) {
-        if(type == null)
-            throw new IllegalArgumentException("Type cannot be null.");
-        
-        this.type = type;
-    }
-    
-    public String getType() {
-        if(type == null)
-            throw new IllegalStateException("Type has not yet been set prior to retrieval.");
-        
-        return type;
-    }
-    
-    public void setPriority(int priority) {
-        this.priority = Integer.valueOf(priority);
-    }
-    
+
+    /**
+     * Get the priority of the service.
+     * 
+     * @return The priority of the service; if {@code null}, this has not been set.
+     */
     public Integer getPriority() {
         return priority;
     }
+
+    /**
+     * Get the type URL associated with the service.
+     * 
+     * @return The type URL.
+     * @throws IllegalStateException
+     *             If the type URL has not yet been {@link #setType(String) set}.
+     */
+    public String getType() {
+        if (type == null)
+            throw new IllegalStateException("Type has not yet been set prior to retrieval.");
     
+        return type;
+    }
+
+    /**
+     * Get the URI for the discovered service.
+     * 
+     * @return The URI of the service.
+     * @throws IllegalStateException
+     *             If the URI has not yet been {@link #setUri(String)}.
+     */
+    public String getUri() {
+        if (uri == null)
+            throw new IllegalStateException("URI has not yet been set prior to retrieval.");
+    
+        return uri;
+    }
+
+    /**
+     * Set the regular expression used to match against the request against the server.
+     * 
+     * @param hostRegex
+     *            The regular expression to match against the request path.
+     * @throws IllegalArgumentException
+     *             If the given expression is {@code null}.
+     */
+    public void setHostRegex(String hostRegex) {
+        if (hostRegex == null)
+            throw new IllegalArgumentException("Host regular expression cannot be null.");
+    
+        this.hostRegex = Pattern.compile(hostRegex);
+    }
+
+    /**
+     * Set the content of the {@code <LocalID />} element in a service XRDS element.
+     * 
+     * @param localId
+     *            The local ID.
+     * @throws IllegalArgumentException
+     *             If the given local ID is {@code null}.
+     */
     public void setLocalId(String localId) {
-        if(localId == null)
+        if (localId == null)
             throw new IllegalArgumentException("Local ID cannot be null.");
-        
+
         this.localId = localId;
     }
+
+    /**
+     * Set the priority of the service.
+     * 
+     * @param priority
+     *            The priority of the service.
+     */
+    public void setPriority(int priority) {
+        this.priority = Integer.valueOf(priority);
+    }
+
+    /**
+     * Set the type URL of the discovered service.
+     * 
+     * @param type
+     *            The type URL.
+     * @throws IllegalArgumentException
+     *             If the given URL is {@code null}.
+     */
+    public void setType(String type) {
+        if (type == null)
+            throw new IllegalArgumentException("Type cannot be null.");
     
-    public String getLocalId() {
-        return localId;
+        this.type = type;
+    }
+
+    /**
+     * Set the value of the {@code <URI />} tag for a service.
+     * 
+     * @param uri
+     *            The URI of the service.
+     * @throws IllegalArgumentException
+     *             If the given URI is {@code null}.
+     */
+    public void setUri(String uri) {
+        if (uri == null)
+            throw new IllegalArgumentException("URI cannot be null.");
+    
+        this.uri = uri;
     }
 }
--- trunk/openid-discovery-maven-plugin/src/main/java/com/google/code/openid/mojo/jetty/DiscoveredServiceHandler.java
+++ trunk/openid-discovery-maven-plugin/src/main/java/com/google/code/openid/mojo/jetty/DiscoveredServiceHandler.java
@@ -15,20 +15,44 @@ import org.mortbay.jetty.handler.AbstractHandler;
 import com.google.code.openid.mojo.DiscoveredService;
 import com.google.code.openid.mojo.openid.DiscoveredServiceWriter;
 
+/**
+ * An {@link AbstractHandler} used to serve out information about a requested service.
+ * 
+ * @author jrh3k5
+ * 
+ */
+
 public class DiscoveredServiceHandler extends AbstractHandler {
     private final Collection<DiscoveredService> services;
     private final DiscoveredServiceWriter writer;
     private final String xriCanonicalId;
-    
+
+    /**
+     * Create a handler.
+     * 
+     * @param xriCanonicalId
+     *            The optional canonical ID to be used in the served-out discovery document; this can be {@code null}.
+     * @param services
+     *            A {@link Collection} of {@link DiscoveredService} objects that represent the services to be handled by this handler.
+     */
     public DiscoveredServiceHandler(String xriCanonicalId, Collection<DiscoveredService> services) {
         this(xriCanonicalId, services, new DiscoveredServiceWriter());
     }
 
-    public DiscoveredServiceHandler(String xriCanonicalId, Collection<DiscoveredService> services,
-            DiscoveredServiceWriter writer) {
-        if(services == null)
+    /**
+     * Create a handler.
+     * 
+     * @param xriCanonicalId
+     *            The optional canonical ID to be used in the served-out discovery document; this can be {@code null}.
+     * @param services
+     *            A {@link Collection} of {@link DiscoveredService} objects that represent the services to be handled by this handler.
+     * @param writer
+     *            A {@link DiscoveredServiceWriter} used to write out information about the services matching the request.
+     */
+    public DiscoveredServiceHandler(String xriCanonicalId, Collection<DiscoveredService> services, DiscoveredServiceWriter writer) {
+        if (services == null)
             throw new IllegalArgumentException("Services cannot be null.");
-        
+
         if (writer == null)
             throw new IllegalArgumentException("Writer cannot be null.");
 
@@ -40,16 +64,13 @@ public class DiscoveredServiceHandler extends AbstractHandler {
     /**
      * {@inheritDoc}
      */
-    public void handle(String target, HttpServletRequest request, HttpServletResponse response,
-            int dispatch) throws IOException, ServletException {
-        // TODO: remove
-        System.out.println("Target is: " + target);
+    public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) throws IOException, ServletException {
         final List<DiscoveredService> matches = new LinkedList<DiscoveredService>();
-        for(DiscoveredService service : services)
-            if(service.getHostRegex().matcher(target).matches())
+        for (DiscoveredService service : services)
+            if (service.getHostRegex().matcher(target).matches())
                 matches.add(service);
-        
-        if(!matches.isEmpty()) {
+
+        if (!matches.isEmpty()) {
             response.setHeader("content-type", "application/xrds+xml");
             response.setStatus(HttpServletResponse.SC_OK);
             writer.write(xriCanonicalId, matches, response.getOutputStream());
